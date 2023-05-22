6ec66d4ba8f8efa5c6530fead9f0d1304c3ca4c1
yusufobr <yousarsif80@gmail.com>
2023-05-22 16:27:51 +0100

    create header with navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
+ 
+const Navbar = () => {
+  return (
+    <header className='container flex items-center justify-between mx-auto mt-4 pb-6 border-b-2'>
+        <div className='flex items-center gap-4'>
+        <a>
+            <img width={50} src={logo} alt='logo'/>
+        </a>
+        <span className='text-3xl font-semibold'>space travelers' hub</span>
+
+        </div>
+        <nav className='flex gap-4 text-blue-400'>
+            <NavLink to='/'>Rockets</NavLink>
+            <NavLink to='/missions'>Missions</NavLink>
+            <span className='w-[1px] h-[24px] bg-gray-400'></span>
+            <NavLink to='/myprofile'>My profile</NavLink>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
