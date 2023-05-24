0ed9e6ae9b2369b5085a6679e6872b96414a1018
Isabella Otoo <isabellaotoo25@gmail.com>
2023-05-24 14:02:56 +0000

    Display reserved rockets in myprofile

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -3,8 +3,12 @@ import { useSelector } from 'react-redux';
 
 function Myprofile() {
   const { missions } = useSelector((state) => state.missions);
+  const { rockets } = useSelector((state) => state.rockets);
   const reservedMissions = missions.filter(
     (mission) => mission.reserved === true,
+  );
+  const reservedRockets = rockets.filter(
+    (rocket) => rocket.reserved === true,
   );
 
   return (
@@ -26,7 +30,15 @@ function Myprofile() {
       <div>
         <span className="text-2xl font-semibold">My Rockets</span>
         <ul className="mt-4">
-          <li className="text-xl h-12 border p-2">test</li>
+          {reservedRockets.length > 0 ? (
+            reservedRockets.map((item) => (
+              <li key={item.rocket_id} className="text-xl h-12 border p-2">
+                {item.rocket_name}
+              </li>
+            ))
+          ) : (
+            <li>No Rocket</li>
+          )}
         </ul>
       </div>
     </div>
