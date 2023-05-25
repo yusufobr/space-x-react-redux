ae7695eaf3d045de5b0650d04eaf17f60baa2ed3
yusufobr <yousarsif80@gmail.com>
2023-05-25 17:45:29 +0100

    remove useless files & move Rockets.js to __test__ folder

--- src/components/__test__/Rockets.test.js
+++ src/components/__test__/Rockets.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import Rockets from './components/Rockets';
+import Rockets from '../Rockets';
 
 const mockStore = configureStore([]);
 const initialState = {
